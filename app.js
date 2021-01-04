059b05df2702c6c8e0da97259f039f899b72d1cb
Macbook <trieungochai.dev@gmail.com>
2021-01-04 18:31:37 +0700

    Getting input from users

---
+++ app.js
@@ -0,0 +1,11 @@
+const getNotes = require('./notes');
+
+const command =  process.argv[2];
+
+if (command === 'add') {
+  console.log('Adding note!');
+} else if (command === 'remove') {
+  console.log('Removing note!');
+};
+
+console.log(process.argv);
